0f84d13dfe76f43a971b52311d12c9609c91d1bc
EsinShadrach <esinshadrachrafe@gmail.com>
2024-01-05 20:40:58 +0100

    Edited package.json added prisma generate

--- utils/actions/user/get-users.ts
+++ utils/actions/user/get-users.ts
@@ -2,6 +2,7 @@
 import prisma from "~/utils/prisma";
 
 export async function getUsers() {
+  // console.log(prisma);
   if (prisma) {
     console.log("============ getting users ============");
     const users = await prisma.user.findMany();
